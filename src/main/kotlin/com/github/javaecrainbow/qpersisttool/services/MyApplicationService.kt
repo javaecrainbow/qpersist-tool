870d51cb96eb8581f33d09ad457ba38b0ab34346
GitHub Action <action@github.com>
2022-11-14 09:13:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/javaecrainbow/qpersisttool/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/javaecrainbow/qpersisttool/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.javaecrainbow.qpersisttool.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.javaecrainbow.qpersisttool.MyBundle
 
 class MyApplicationService {
 
